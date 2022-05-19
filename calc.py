38ac3b44683ea1fe12d0e8c9d0bb23c2d3a72d5b
jeffery Mativo <riikajeffery@gmail.com>
2022-05-19 21:27:28 +0300

    stringsinpython

---
+++ calc.py
@@ -0,0 +1,4 @@
+f_num = input("enter the first number: ")
+s_num = input("enter the second number: ")
+sum_of_nums = int(f_num) + int(s_num)
+print("the sum of the 2 numbers is " +sum_of_nums)
---
+++ strings.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentDefaultsHelpFormatter
+from poplib import POP3_PORT
+from re import X
+
+
+msg='''QRS3X6 Confirmed hku have received ksh1200'''
+print(msg)
+print(msg)
+city = "Nairobi"
+#.upper()to convert to uppercase
+print(city .upper())
+uni = "JKUAT"
+print(uni.lower())
+Fruit = "pineapple"
+print(Fruit[:-1])
+print(Fruit[-2:])
+# strip removes spaces between the words
+f_name = " Bo b A fuata"
+print(f_name.strip())
+p_name = "june"
+s_name = "popo"
+full_name = p_name + s_name
+print(full_name)
+
+city = "Mombasa"
+print(len(city))
+
+word = "Pneumonoultramicroscopicsilicovolcanoconiosis"
+print(len(word))
+
+#int to string 
+
+x = 100
+y = 3.14
+print(str(X))
+print(float(z))
